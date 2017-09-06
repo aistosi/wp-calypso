058512d22e932ff8ba2886affb13fef8036f950a
Justin Shreve <justin.shreve@gmail.com>
2017-09-06 13:36:17 -0700

    Adds state for tracking the current page, product, and search term being viewed by the UI for the reviews list.

---
+++ client/extensions/woocommerce/state/ui/reviews/test/reducer.js
@@ -0,0 +1,111 @@
+/**
+ * External dependencies
+ */
+import { expect } from 'chai';
+import deepFreeze from 'deep-freeze';
+
+/**
+ * Internal dependencies
+ */
+import reducer from '../reducer';
+import { WOOCOMMERCE_UI_REVIEWS_SET_QUERY } from 'woocommerce/state/action-types';
+
+describe( 'reducer', () => {
+	it( 'should have no change by default', () => {
+		const newState = reducer( undefined, {} );
+		expect( newState ).to.eql( {} );
+	} );
+
+	it( 'should store the current query', () => {
+		const action = {
+			type: WOOCOMMERCE_UI_REVIEWS_SET_QUERY,
+			siteId: 123,
+			query: {
+				page: 2,
+			},
+		};
+		const newState = reducer( undefined, action );
+		expect( newState ).to.eql( { 123: { currentPage: 2, currentSearch: '', currentProduct: null } } );
+	} );
+
+	it( 'should update the current page when changed', () => {
+		const action = {
+			type: WOOCOMMERCE_UI_REVIEWS_SET_QUERY,
+			siteId: 123,
+			query: {
+				page: 2,
+			},
+		};
+		const originalState = deepFreeze( { 123: { currentPage: 3, currentSearch: '' } } );
+		const newState = reducer( originalState, action );
+		expect( newState ).to.eql( { 123: { currentPage: 2, currentSearch: '', currentProduct: null } } );
+	} );
+
+	it( 'should update the current search when changed', () => {
+		const action = {
+			type: WOOCOMMERCE_UI_REVIEWS_SET_QUERY,
+			siteId: 123,
+			query: {
+				search: 'testing'
+			},
+		};
+		const originalState = deepFreeze( { 123: { currentPage: 3, currentSearch: '' } } );
+		const newState = reducer( originalState, action );
+		expect( newState ).to.eql( { 123: { currentPage: 3, currentSearch: 'testing', currentProduct: null } } );
+	} );
+
+	it( 'should update the current product when changed', () => {
+		const action = {
+			type: WOOCOMMERCE_UI_REVIEWS_SET_QUERY,
+			siteId: 123,
+			query: {
+				product: 50,
+			},
+		};
+		const originalState = deepFreeze( { 123: { currentPage: 3, currentSearch: '' } } );
+		const newState = reducer( originalState, action );
+		expect( newState ).to.eql( { 123: { currentPage: 3, currentSearch: '', currentProduct: 50 } } );
+	} );
+
+	it( 'should store the current query for more than one site', () => {
+		const action = {
+			type: WOOCOMMERCE_UI_REVIEWS_SET_QUERY,
+			siteId: 234,
+			query: {
+				search: 'testing'
+			},
+		};
+		const originalState = deepFreeze( { 123: { currentPage: 3, currentSearch: '', currentProduct: null } } );
+		const newState = reducer( originalState, action );
+		expect( newState ).to.eql( {
+			123: { currentPage: 3, currentSearch: '', currentProduct: null },
+			234: { currentPage: 1, currentSearch: 'testing', currentProduct: null }
+		} );
+	} );
+
+	it( 'should remove the key when page is re-set to initial state', () => {
+		const action = {
+			type: WOOCOMMERCE_UI_REVIEWS_SET_QUERY,
+			siteId: 123,
+			query: {
+				page: 1,
+			}
+		};
+		const originalState = deepFreeze( { 123: { currentPage: 3, currentSearch: '' } } );
+		const newState = reducer( originalState, action );
+		expect( newState ).to.eql( {} );
+	} );
+
+	it( 'should remove the key when search is re-set to initial state', () => {
+		const action = {
+			type: WOOCOMMERCE_UI_REVIEWS_SET_QUERY,
+			siteId: 123,
+			query: {
+				search: '',
+			}
+		};
+		const originalState = deepFreeze( { 123: { currentPage: 1, currentSearch: 'test' } } );
+		const newState = reducer( originalState, action );
+		expect( newState ).to.eql( {} );
+	} );
+} );
